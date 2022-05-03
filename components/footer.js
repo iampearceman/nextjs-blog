904727dfedfe5729bf810b09b5e87e56df59cbad
StarL0rdDude <63902456+StarL0rdDude@users.noreply.github.com>
2022-05-03 07:58:00 +0300

    changed footer

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,8 @@ export default function Footer() {
     return (
         <div className='bg-fuchsia-100 mt-8 py-4'>
             <div className='container mx-auto flex justify-center'>
-                &copy; 2022 DailyDevTips
+                &copy; Made with ❤️ by Emillien Pearce.
+
             </div>
         </div>
     )
